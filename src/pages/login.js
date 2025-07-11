291a72d9d35a859a550d20aca3ebef846605fc41
Vasu-Thalluri <76046184+Vasu-Thalluri@users.noreply.github.com>
2025-07-11 21:04:39 +0530

    updated functionality for login and signup

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,64 @@
 import React from 'react'
+import { useState } from 'react';
+import {useDispatch} from 'react-redux';
+import {login} from '../redux/slices/userSlice';
+import { jsonApi } from '../services/api';
+import { useNavigate } from 'react-router-dom';
 
 const Login = ()=> {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [successMsg, setSuccessMsg] = useState('');
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const value = e.target;
+    console.log(typeof(value));
+    setError('');
+    getUser();
+  }
+
+  function getUser() {
+    jsonApi.get('/users')
+      .then(response => {
+        console.log(response);
+        console.log('API finished');// This runs after the API call finishes. This cosole is for how asynchronous code works
+        const users = Array.isArray(response.data) ? response.data : [];
+        const matchedUser = users.find(
+          user => user.email === email && user.password === password
+        );
+        if (matchedUser) {
+          console.log('Login successful');
+          localStorage.setItem('user', JSON.stringify({ email }));
+          setEmail(matchedUser.email);
+          dispatch(login({ email }));
+          setError('');
+          setSuccessMsg('Login successful');
+          navigate('/home'); // navigate to home
+        } else {
+          setError('Invalid email or password');
+        }
+      }).catch(error => {
+        console.error('Error fetching users:', error);
+        setError('An error occurred while logging in');
+      });
+      console.log('This runs immediately after jsonApi.get, before the API finishes');
+  }
+  
   return (
-    //<div>
-        <h1>Login Page</h1>
-    //</div>
+    <div>
+        <h1>Login</h1>
+        <form onSubmit = {handleLogin}>
+          <input type='email' name='email' value={email} onChange={(e)=> setEmail(e.target.value)} placeholder = "Enter Your Email here" required/>
+          <input type= 'password' name='password' value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Enter Your Password here" required/>
+          <button type="submit">Login</button>
+          {error && <p style={{color: 'red'}}>{error}</p>}
+          {successMsg && <p style={{color: 'green'}}>{successMsg}</p>}
+        </form>
+    </div>
   )
 }
 export default Login;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState } from 'react';
+import { jsonApi } from '../services/api';
+import { useDispatch } from 'react-redux';
+import { login } from '../redux/slices/userSlice';
 
 const Signup = ()=> {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSignup = async (e)=> {
+    e.preventDefault();
+    try {
+      const res = await jsonApi.post('/users', {name, email, password});
+      console.log(res)
+      console.log(res.data);
+      dispatch(login({email}));
+    }catch(error) {
+      console.error('Signup failed:', error);
+    }
+  }
+
   return (
     <div>
-        <h1>Signup Page</h1>
+        <h1>Signup</h1>
+        <form onSubmit={handleSignup}>
+          <input type='text' name='name' placeholder='Enter Your Name' required value={name} onChange={(e)=> setName(e.target.value)}/>
+          <input type='email' name='email' placeholder='Enter Your Email' required value={email} onChange={(e)=> setEmail(e.target.value)}/>
+          <input type='password' name='password' placeholder='Enter Your password' required value={password} onChange={(e)=> setPassword(e.target.value)}/>
+          <button type='submit'>Signup</button>  
+        </form>
     </div>
   )
 }
